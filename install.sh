9aed517640d9d9dd7199577dca49c6dcf66d7022
wnadurski <w.nadurski@gmail.com>
2017-06-25 15:28:26 +0200

    Added colors and install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp .bashrc ~/
+cp .tmux.conf ~/
+cp .vimrc ~/
+cp .minttyrc ~/
+mkdir -p ~/.vim/colors
+cp solarized.vim ~/.vim/colors/
