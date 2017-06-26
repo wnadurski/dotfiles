a70864eef419fc1894f804a9decfd3dff336e266
Wojciech Nadurski <wojciech.nadurski@oke.pl>
2017-06-26 12:26:24 +0200

    Added pretty git log command

--- install.sh
+++ install.sh
@@ -9,3 +9,4 @@ mkdir -p ~/.vim/colors
 cp solarized.vim ~/.vim/colors/
 
 vim +PluginInstall +qall
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
