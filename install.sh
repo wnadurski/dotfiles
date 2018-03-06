0710d176aadf1863db58d5b57d588c5612b04b79
Wojciech Nadurski <wnadurski@atlassian.com>
2018-03-06 14:14:46 +0100

    Linking instead of copying

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp .bashrc ~/
-cp .tmux.conf ~/
-cp .vimrc ~/
+ln -s .bashrc ~/
+ln -s .tmux.conf ~/
+ln -s .vimrc ~/
 #cp .minttyrc ~/
 #cp .dir_colors ~/
 mkdir -p ~/.vim/colors
-cp solarized.vim ~/.vim/colors/
-cp .zshrc ~/
-cp .env.sh ~/
+ln -s solarized.vim ~/.vim/colors/
+ln -s .zshrc ~/
+ln -s .env.sh ~/
 
 vim +PluginInstall +qall
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
